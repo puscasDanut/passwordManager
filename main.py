a72b05649d4558f75cebb65b6b161a8739744072
puscasDanut <danielionut@puscas.xyz>
2023-05-12 18:34:44 +0300

    GUI started

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
+from tkinter import *
+import os
+
+main_dir = os.path.dirname(__file__)
+
 if __name__ == '__main__':
-    pass
+    window = Tk()
+    window.config(padx=20, pady=20)
+    window.title("Pușcaș Password Manager")
+    logo = PhotoImage(file=main_dir+"./logo.png")
+    icon = PhotoImage(file=main_dir+"./icon.png")
+    window.iconphoto(False, icon)
+
+    canvas = Canvas(width=200, height=200)
+    canvas.create_image(100, 100, image=logo)
+    canvas.grid(column=1, row=1)
+
+    window.mainloop()
