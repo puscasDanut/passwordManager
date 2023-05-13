d0e1ba52303c4121af7f9dcc403188411cca4699
puscasDanut <danielionut@puscas.xyz>
2023-05-13 15:55:06 +0300

    Finished project x2

--- main.py
+++ main.py
@@ -15,6 +15,28 @@ if __name__ == '__main__':
     icon = PhotoImage(file=main_dir + "./icon.png")
     window.iconphoto(False, icon)
 
+    def find_credentials():
+        website = website_input.get().title()
+        try:
+            with open(main_dir + "./passwords.json", mode="r") as file:
+                data = json.load(file)
+        except FileNotFoundError:
+            messagebox.showinfo(title="Error", message="You didn't register any passwords!")
+        else:
+            password_available = False
+            if website in data:
+                email = data[website]['email']
+                password = data[website]['password']
+                messagebox.showinfo(title=f"Credentials for {website}", message=f"Username/Email: {email}"
+                                                                                f"\nPassword: {password}")
+                password_available = True
+            if not password_available:
+                messagebox.showinfo(title="Password not available", message=f"You didn't register any credentials for {website}")
+
+    def write_password(data):
+        with open(main_dir + "./passwords.json", mode="w") as file:
+            json.dump(data, file, indent=4)
+
     def fill_password():
         new_password = generate_password()
         password_input.delete(0, END)
@@ -22,7 +44,7 @@ if __name__ == '__main__':
         copy(new_password)
 
     def save_credentials():
-        website = website_input.get()
+        website = website_input.get().title()
         username = username_input.get()
         password = password_input.get()
         new_data = {
@@ -35,13 +57,17 @@ if __name__ == '__main__':
         if website == "" or password == "":
             messagebox.showinfo(title="Error", message="All fields are required!")
         else:
-            with open(main_dir + "./passwords.json", mode="r") as file:
-                data = json.load(file)
+            try:
+                with open(main_dir + "./passwords.json", mode="r") as file:
+                    data = json.load(file)
+            except FileNotFoundError:
+                write_password(new_data)
+            else:
                 data.update(new_data)
-            with open(main_dir + "./passwords.json", "w") as file:
-                json.dump(data, file, indent=4)
-            website_input.delete(0, END)
-            password_input.delete(0, END)
+                write_password(data)
+            finally:
+                website_input.delete(0, END)
+                password_input.delete(0, END)
 
 
     canvas = Canvas(width=200, height=200)
@@ -51,25 +77,28 @@ if __name__ == '__main__':
     website_label = Label(text="Website:")
     website_label.grid(column=0, row=1)
 
-    website_input = Entry(width=35)
-    website_input.grid(column=1, row=1, columnspan=2)
+    website_input = Entry(width=30)
+    website_input.grid(column=1, row=1)
     website_input.focus()
+
+    search_button = Button(text="Search", command=find_credentials)
+    search_button.grid(column=2, row=1, padx=(0, 60))
 
     username_label = Label(text="Email/Username:")
     username_label.grid(column=0, row=2)
 
     username_input = Entry(width=35)
-    username_input.grid(column=1, row=2, columnspan=2)
+    username_input.grid(column=1, row=2, columnspan=2, padx=(0, 85))
     username_input.insert(0, "username@your.domain")
 
     password_label = Label(text="Password:")
     password_label.grid(column=0, row=3)
 
-    password_input = Entry(width=21, show="*")
+    password_input = Entry(width=30, show="*")
     password_input.grid(column=1, row=3)
 
     password_generate = Button(text="Generate", height=1, command=fill_password)
-    password_generate.grid(column=2, row=3, padx=(0, 30))
+    password_generate.grid(column=2, row=3, padx=(0, 60))
 
     add_credentials = Button(text="Add Credentials", width=36, command=save_credentials)
     add_credentials.grid(column=0, row=4, columnspan=3)
